ffc3c1043ea880027967b9d3b0a8a48ffca5c585
anighosal <121676026+anighosal@users.noreply.github.com>
2023-06-07 00:00:27 +0600

    main layout created

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./Routes/Routes";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>

2e1481f1ad515292d16d96c2c26b39a72b2f56b9
anighosal <121676026+anighosal@users.noreply.github.com>
2023-06-07 01:17:35 +0600

    carosusel added

---
+++ src/pages/Home/Banner/Banner.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
+import img1 from "../../../assets/1banner.jpg";
+import img2 from "../../../assets/img2.avif";
+import img3 from "../../../assets/img3.avif";
+
+const Banner = () => {
+  return (
+    <Carousel>
+      <div>
+        <img src={img1} />
+      </div>
+      <div>
+        <img src={img2} />
+      </div>
+      <div>
+        <img src={img3} />
+      </div>
+    </Carousel>
+  );
+};
+
+export default Banner;

d279d22b07982bdf4fbf09662a058457c56f6041
anighosal <121676026+anighosal@users.noreply.github.com>
2023-06-08 16:02:35 +0600

    my select helmet added

--- src/pages/Dashboard/MySelect/MySelect.jsx
+++ src/pages/Dashboard/MySelect/MySelect.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import { Helmet } from "react-helmet-async";
 
 const MySelect = () => {
   return (
     <div>
+      <Helmet>
+        <title>Music King | My selected Classes</title>
+      </Helmet>
       <h2>hello</h2>
     </div>
   );

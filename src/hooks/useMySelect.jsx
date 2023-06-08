1ecee89b2ac245d7bf5edf6ea7bdc83d7f24978b
anighosal <121676026+anighosal@users.noreply.github.com>
2023-06-08 20:21:07 +0600

    tanstack added

--- src/hooks/useMySelect.jsx
+++ src/hooks/useMySelect.jsx
@@ -1,5 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { AuthContext } from "../providers/AuthProviders";
 
-const useMySelect = (email) => {};
+const useMySelect = () => {
+  const { user } = useContext(AuthContext);
+
+  const { refetch, data: classes = [] } = useQuery({
+    queryKey: ["classes", user?.email],
+    queryFn: async () => {
+      const res = await fetch(
+        `https://my-12th-work-server.vercel.app/classes?email=${user.email}`
+      );
+      return res.json();
+    },
+  });
+  return [classes, refetch];
+};
 
 export default useMySelect;
--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,9 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./Routes/Routes";
 import { HelmetProvider } from "react-helmet-async";
 import AuthProviders from "./providers/AuthProviders";
-import {
-  useQuery,
-  useMutation,
-  useQueryClient,
-  QueryClient,
-  QueryClientProvider,
-} from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>

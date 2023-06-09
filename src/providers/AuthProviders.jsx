f66096b2f4a905c7811c8ee8acc8a3b214eb989b
anighosal <121676026+anighosal@users.noreply.github.com>
2023-06-09 15:06:52 +0600

    local storage set and remove method done

--- src/providers/AuthProviders.jsx
+++ src/providers/AuthProviders.jsx
@@ -59,8 +59,11 @@ const AuthProviders = ({ children }) => {
             email: currentUser.email,
           })
           .then((data) => {
-            console.log(data);
+            console.log(data.data.token);
+            localStorage.setItem("access-token", data.data.token);
           });
+      } else {
+        localStorage.removeItem("access-token");
       }
 
       setLoading(false);

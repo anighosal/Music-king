c67e22dac869f099c3b00a2adb77164e57f0d48d
anighosal <121676026+anighosal@users.noreply.github.com>
2023-06-09 09:31:24 +0600

    sort added

--- src/pages/Home/PopularClass/PopularClass.jsx
+++ src/pages/Home/PopularClass/PopularClass.jsx
@@ -3,17 +3,25 @@ import PopularClassCard from "./PopularClassCard";
 
 const PopularClass = () => {
   const [popularAllClass, setPopularAllClass] = useState([]);
+  const [asc, setAsc] = useState(true);
 
   useEffect(() => {
-    fetch("https://my-12th-work-server.vercel.app/musicData")
+    fetch(
+      `https://my-12th-work-server.vercel.app/musicData?sort=${
+        asc ? "asc" : "desc"
+      }`
+    )
       .then((res) => res.json())
       .then((data) => setPopularAllClass(data));
-  }, []);
+  }, [asc]);
   return (
-    <div>
+    <div className="text-center">
       <h2 className="text-center text-3xl font-bold text-blue-700 mb-4">
         ----Our Popular top 6 class---
       </h2>
+      <button className="btn btn-primary" onClick={() => setAsc(!asc)}>
+        {asc ? "Price: High to low" : "Price:Low to High"}
+      </button>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {popularAllClass.map((allclass) => (
           <PopularClassCard

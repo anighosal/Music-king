6f3b7b587572a5ce543f77e21720eebb87487aa2
anighosal <121676026+anighosal@users.noreply.github.com>
2023-06-11 23:55:54 +0600

    instructor home done

--- src/pages/Dashboard/InstructorHome/InstructorHome.jsx
+++ src/pages/Dashboard/InstructorHome/InstructorHome.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import useAuth from "../../../hooks/useAuth";
 
 const InstructorHome = () => {
+  const { user } = useAuth();
   return (
-    <div>
-      <h3>instructor home</h3>
+    <div className="w-full m-4 ml-28">
+      <img
+        className="rounded w-[200px] 
+    "
+        src={user.photoURL}
+        alt=""
+      />
+      <h2 className="text-3xl mt-2">Name: {user.displayName}</h2>
+      <h2 className="text-3xl">Email: {user.email}</h2>
+      <h2 className="text-3xl">Instructor</h2>
     </div>
   );
 };
